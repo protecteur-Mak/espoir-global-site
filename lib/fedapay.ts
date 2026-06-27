bf183fdc409eecd1ef81b8f89b7ad4acfe2156d7
protecteur-Mak <ongsoutienplus1@gmail.com>
2026-06-27 10:48:39 +0000

    Update fedapay.ts

--- lib/fedapay.ts
+++ lib/fedapay.ts
@@ -1,14 +1,19 @@
-// lib/fedapay.js
+// lib/fedapay.ts
 import { FedaPay } from 'fedapay';
 
-// Initialisation avec la clé publique stockée en variable d'environnement
-FedaPay.setPublicKey(process.env.NEXT_PUBLIC_FEDAPAY_PUBLIC_KEY);
-
-export const initiatePayment = (amount, description) => {
-  return FedaPay.init({
-    public_key: process.env.NEXT_PUBLIC_FEDAPAY_PUBLIC_KEY,
-    transaction: {
-      amount: amount,
-      description: description
-    }
-  });
+export const initiatePayment = (amount: number, description: string) => {
+  // Vérification : s'assurer que le code ne tourne que dans le navigateur
+  if (typeof window !== 'undefined') {
+    FedaPay.setPublicKey(process.env.NEXT_PUBLIC_FEDAPAY_PUBLIC_KEY || '');
+    
+    FedaPay.init({
+      public_key: process.env.NEXT_PUBLIC_FEDAPAY_PUBLIC_KEY || '',
+      transaction: {
+        amount: amount,
+        description: description
+      }
+    });
+  } else {
+    console.warn("FedaPay ne peut être initialisé que côté client.");
+  }
+};
