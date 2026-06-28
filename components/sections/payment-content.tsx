4038bdec7897c23e765424dbe1b26348fd6118b3
protecteur-Mak <ongsoutienplus1@gmail.com>
2026-06-28 10:22:26 +0000

    Update payment-content.tsx

--- components/sections/payment-content.tsx
+++ components/sections/payment-content.tsx
@@ -70,7 +70,7 @@ export function PaymentContent() {
   const EURTOXOF = 655;
   const finalAmount = selectedAmount || Number.parseFloat(customAmount) || 0;
   const finalAmountXOF = Math.round(finalAmount * EURTOXOF);
-  const predefinedAmounts = [2, 5, 10, 20, 50, 100, 200, 300, 500];
+  const predefinedAmounts = [2, 5, 10, 20, 50, 100, 200, 300, 500,1000];
 
   useEffect(() => {
     const amount = new URLSearchParams(window.location.search).get("amount");
