3f70b0a0c62ec03949af06990ec5666f6998a4d3
protecteur-Mak <ongsoutienplus1@gmail.com>
2026-07-15 07:13:21 +0000

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#4f46e5",
     icons: [
       {
-        src: "/icon.png",
+        src: "/logo.png",
         sizes: "192x192",
         type: "image/png",
       },
